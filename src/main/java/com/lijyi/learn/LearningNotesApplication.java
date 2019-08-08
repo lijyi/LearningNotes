b9a20d3aa08df652fc628d06aa12ceb7663b958c
李阳 <>
2019-08-08 22:47:28 +0800

    init

---
+++ src/main/java/com/lijyi/learn/LearningNotesApplication.java
@@ -0,0 +1,16 @@
+package com.lijyi.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liyang
+ * @date 2019-08-08
+ */
+@SpringBootApplication
+public class LearningNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningNotesApplication.class, args);
+    }
+}
